c76b471e48f4bbcf4dddc02bbb3807afefdf9018
1wooseok <74036731+1wooseok@users.noreply.github.com>
2021-03-08 21:12:51 +0900

    Update BaekJoon10809.java

--- BaekJoon/BaekJoon10809.java
+++ BaekJoon/BaekJoon10809.java
@@ -2,30 +2,31 @@ package hello;
 
 import java.util.Scanner;
 
-public class test2 {
+public class BaekJoon10809 {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-
+		
+		String S = sc.next();
 		int[] arr = new int[26];
-		for(int i=0; i<arr.length; i++) {
+		
+		for(int i=0; i<arr.length; i++) {  // 초기화
 			arr[i] = -1;
 		}
 		
-		String S = sc.next(); 
-		
-		for(int i=0; i<S.length(); i++) {
-			char ch = S.charAt(i);   
-			int num = (int)ch;
-			
-			if(arr[num-97] == -1) {
-				arr[num-97] = i;
+		for(int i=0; i<S.length(); i++) {  // 알파벳을 배열의 인덱스로 표현
+			char ch = S.charAt(i);
+			if(arr[ch-'a'] == -1) {
+				arr[ch-'a'] = i;
 			}
-		}	
-		for(int i=0; i<arr.length; i++) {
-			System.out.print(arr[i]+" ");
-		}	
+		}
+		
+		for(int i=0; i<arr.length; i++) {  // 출력
+			System.out.println(arr[i]);
+		}
 	}
 }
 
+
+//  같은 문자 예외 처리
